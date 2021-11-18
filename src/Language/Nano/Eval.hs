b1c7200b6ab036c20fe60c2b57d451a0823c1ade
Edward8484 <52609641+Edward8484@users.noreply.github.com>
2021-11-17 22:28:49 -0800

    Update Eval.hs

--- src/Language/Nano/Eval.hs
+++ src/Language/Nano/Eval.hs
@@ -200,18 +200,18 @@ evalOp Eq (VNil VNil) = VBool (True)
 evalOp Ne (VInt x) (VInt y) = VBool(x/=y)
 evalOp Ne (VBool x) (VBool y) = VBool(x/=y)
 
-evalOp Lt (VInt x) (VInt y) = VBool (x < y)
-evalOp Le (VInt x) (VInt y) = VBool (x <= y)
-evalOp Gt (VInt x) (VInt y) = VBool (x > y)
-evalOp Ge (VInt x) (VInt y) = VBool (x >= y)
+evalOp Lt (VInt x) (VInt y) = VBool (x<y)
+evalOp Le (VInt x) (VInt y) = VBool (x<=y)
+evalOp Gt (VInt x) (VInt y) = VBool (x>y)
+evalOp Ge (VInt x) (VInt y) = VBool (x>=y)
 
-evalOp And (VBool x) (VBool y) = VBool (x && y)
-evalOp Or (VBool x) (VBool y) = VBool (x || y)
+evalOp And (VBool x) (VBool y) = VBool (x&&y)
+evalOp Or (VBool x) (VBool y) = VBool(x||y)
 
-evalOp VNil (VPair x y) = VBool (x == VNil)
-evalOp (VPair x y) VNil = VBool (x == VNil)
+evalOp VNil (VPair x y) = VBool (x==VNil)
+evalOp (VPair x y) VNil = VBool (x==VNil)
 
-evalOp (VPair x y) (VPair a b) = VBool ((x == a) && (y == b))
+evalOp (VPair x y) (VPair a b) = VBool ((x==a) && (y==b))
 
 evalOp Cons elem VNil = VPair elem VNil
 evalOp Cons x y = VPair x y 
